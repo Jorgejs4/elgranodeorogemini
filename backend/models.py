a698e3f5761cc1fc51494565aa711ca0b950bfae
Jorgejs4 <gerogezapatiesta@gmail.com>
2026-03-07 03:18:22 +0100

    actualizando models.py

--- backend/models.py
+++ backend/models.py
@@ -43,7 +43,7 @@ class Interaction(Base):
     product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
     action = Column(String, default="view") 
-
+    action_type = Column(String, default="view")
     user = relationship("User", back_populates="interactions")
     product = relationship("Product", back_populates="interactions")
 
